56df8c9e2655539aac0686fcd68c8fb5775f6b12
OTHSCS077 <OTHSCS077@KATYISD.ORG>
2019-04-12 07:31:07 -0500

    basic character fuctions

---
+++ Dielingo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero; //8:26 in ep 2
+
+    [SerializeField]
+    private float speed;
+    private float lookSensitivity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //Calculate movement velocity as a 3D vector
+        float xMov = Input.GetAxis("Horizontal");
+        float zMov = Input.GetAxis("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMov;
+        Vector3 moveVertical = transform.forward * zMov;
+
+        //Final calculated movement
+        velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        //Calculate rotation as a 3D vector
+        float yRot = Input.GetAxis("Mouse X");
+
+        Vector3 rotation = new Vector3(0f, yRot, 0f) * lookSensitivity;
+    }
+
+    void FixedUpdate()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+}
+/*movement = speed* (transform.forward* Input.GetAxis("Vertical") + transform.right* Input.GetAxis("Horizontal"));
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);*/
