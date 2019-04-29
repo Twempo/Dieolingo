ed02eebf23fb5e3c77ead6a8796f026a81fa8fa1
OTHSCS077 <OTHSCS077@KATYISD.ORG>
2019-04-29 07:50:10 -0500

    character controller complete inv system half complete

---
+++ Dielingo/Assets/Scripts/Inventory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    List<Interactable> inv = new List<Interactable>();
+
+    public void addItem(Interactable obj)
+    {
+        inv.Add(obj);
+        Debug.Log(inv.Count);
+    }
+}
