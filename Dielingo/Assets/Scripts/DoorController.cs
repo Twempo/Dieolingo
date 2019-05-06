23720134bf252b2b3682558a1bed6eaf49c8ecc3
OTHSCS079 <OTHSCS079@KATYISD.ORG>
2019-05-06 07:55:11 -0500

    house done with no windows

---
+++ Dielingo/Assets/Scripts/DoorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //storer.FrontDoor.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
