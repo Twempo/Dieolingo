77f17cfca5bcdfd6bfd83d1e9401defb05050f99
OTHSCS079 <OTHSCS079@KATYISD.ORG>
2019-05-07 07:47:29 -0500

    Doors finished

--- Dielingo/Assets/Scripts/DoorController.cs
+++ Dielingo/Assets/Scripts/DoorController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     public Animator anim;
+    public bool open;
     // Start is called before the first frame update
     void Start()
     {
         //storer.FrontDoor.SetActive(true);
+        open = true;
     }
 
     // Update is called once per frame
